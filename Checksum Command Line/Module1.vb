a82dc4663a9f3cd031194b963274dd5b52f0e945
Thomas Parkison <trparky@toms-world.org>
2024-11-12 12:14:14 -0500

    Rewrote everything.

--- Checksum Command Line/Module1.vb
+++ Checksum Command Line/Module1.vb
@@ -1,4 +1,6 @@
-﻿Imports System.Text.RegularExpressions
+﻿Imports System.Collections.ObjectModel
+Imports System.Net.Security
+Imports System.Text.RegularExpressions
 
 Module Module1
     Enum HashType
@@ -12,105 +14,130 @@ Module Module1
         crc32 = 7
     End Enum
 
-    Public Function ComputeHash(ByVal filename As String, Optional mode As HashType = HashType.sha160) As String
+    Private Function ComputeHash(filename As String, Optional mode As HashType = HashType.sha160) As String
         Dim byteOutput As Byte()
 
         Using fileStreamObject As New IO.FileStream(filename, IO.FileMode.Open, IO.FileAccess.Read, IO.FileShare.Read, 10 * 1048576, IO.FileOptions.SequentialScan)
             If mode = HashType.md5 Then
-                Dim hashEngine As New Security.Cryptography.MD5CryptoServiceProvider
-                byteOutput = hashEngine.ComputeHash(fileStreamObject)
-                hashEngine.Dispose()
+                Using hashEngine As New Security.Cryptography.MD5CryptoServiceProvider
+                    byteOutput = hashEngine.ComputeHash(fileStreamObject)
+                End Using
             ElseIf mode = HashType.sha160 Then
-                Dim hashEngine As New Security.Cryptography.SHA1CryptoServiceProvider
-                byteOutput = hashEngine.ComputeHash(fileStreamObject)
-                hashEngine.Dispose()
+                Using hashEngine As New Security.Cryptography.SHA1CryptoServiceProvider
+                    byteOutput = hashEngine.ComputeHash(fileStreamObject)
+                End Using
             ElseIf mode = HashType.sha256 Then
-                Dim hashEngine As New Security.Cryptography.SHA256CryptoServiceProvider
-                byteOutput = hashEngine.ComputeHash(fileStreamObject)
-                hashEngine.Dispose()
+                Using hashEngine As New Security.Cryptography.SHA256CryptoServiceProvider
+                    byteOutput = hashEngine.ComputeHash(fileStreamObject)
+                End Using
             ElseIf mode = HashType.sha384 Then
-                Dim hashEngine As New Security.Cryptography.SHA384CryptoServiceProvider
-                byteOutput = hashEngine.ComputeHash(fileStreamObject)
-                hashEngine.Dispose()
+                Using hashEngine As New Security.Cryptography.SHA384CryptoServiceProvider
+                    byteOutput = hashEngine.ComputeHash(fileStreamObject)
+                End Using
             ElseIf mode = HashType.sha512 Then
-                Dim hashEngine As New Security.Cryptography.SHA512CryptoServiceProvider
-                byteOutput = hashEngine.ComputeHash(fileStreamObject)
-                hashEngine.Dispose()
+                Using hashEngine As New Security.Cryptography.SHA512CryptoServiceProvider
+                    byteOutput = hashEngine.ComputeHash(fileStreamObject)
+                End Using
             ElseIf mode = HashType.ripemd160 Then
-                Dim hashEngine As New Security.Cryptography.RIPEMD160Managed
-                byteOutput = hashEngine.ComputeHash(fileStreamObject)
-                hashEngine.Dispose()
+                Using hashEngine As New Security.Cryptography.RIPEMD160Managed
+                    byteOutput = hashEngine.ComputeHash(fileStreamObject)
+                End Using
             Else
-                Dim hashEngine As New Security.Cryptography.SHA1CryptoServiceProvider
-                byteOutput = hashEngine.ComputeHash(fileStreamObject)
-                hashEngine.Dispose()
+                Using hashEngine As New Security.Cryptography.SHA1CryptoServiceProvider
+                    byteOutput = hashEngine.ComputeHash(fileStreamObject)
+                End Using
             End If
         End Using
 
         Return BitConverter.ToString(byteOutput).ToLower().Replace("-", "")
     End Function
 
-    Sub Main()
-        Dim commandLineEntry As String
-        Dim htHashType As HashType = HashType.null
-        Dim strFileToBeHashed As String = Nothing
-        Dim boolAsterisk As Boolean = False
+    Private Function ParseArguments(args As ReadOnlyCollection(Of String)) As Dictionary(Of String, Object)
+        Dim parsedArguments As New Dictionary(Of String, Object)(StringComparer.OrdinalIgnoreCase)
+        Dim strValue As String
 
-        For i = 0 To Environment.GetCommandLineArgs().Count - 1
-            commandLineEntry = Environment.GetCommandLineArgs(i)
+        For Each strArgument As String In args
+            If strArgument.StartsWith("--") Then
+                Dim splitArg As String() = strArgument.Substring(2).Split(New Char() {"="c}, 2)
+                Dim key As String = splitArg(0)
 
-            If Regex.IsMatch(commandLineEntry, "\A-hash", RegexOptions.IgnoreCase) Or Regex.IsMatch(commandLineEntry, "\A-h", RegexOptions.IgnoreCase) Then
-                commandLineEntry = Regex.Replace(commandLineEntry, "-(?:hash|h)=", "", RegexOptions.IgnoreCase)
-
-                Select Case commandLineEntry.ToLower.Trim
-                    Case "md5"
-                        htHashType = HashType.md5
-                    Case "sha1"
-                        htHashType = HashType.sha160
-                    Case "sha160"
-                        htHashType = HashType.sha160
-                    Case "sha256"
-                        htHashType = HashType.sha256
-                    Case "sha384"
-                        htHashType = HashType.sha384
-                    Case "sha512"
-                        htHashType = HashType.sha512
-                    Case "ripemd160"
-                        htHashType = HashType.ripemd160
-                    Case Else
-                        Console.WriteLine("ERROR: Invalid hash mode")
-                        Exit Sub
-                End Select
-            ElseIf Regex.IsMatch(commandLineEntry, "\A-file", RegexOptions.IgnoreCase) Then
-                commandLineEntry = Regex.Replace(commandLineEntry, "-file=", "", RegexOptions.IgnoreCase)
-                strFileToBeHashed = commandLineEntry
-            ElseIf commandLineEntry.Equals("-asterisk", StringComparison.OrdinalIgnoreCase) Then
-                boolAsterisk = True
+                If splitArg.Length = 2 Then
+                    ' Argument with a value
+                    strValue = splitArg(1)
+                    parsedArguments(key) = strValue
+                Else
+                    ' Boolean flag
+                    parsedArguments(key) = True
+                End If
+            Else
+                Console.WriteLine($"Unrecognized argument format: {strArgument}")
             End If
         Next
 
-        If htHashType = HashType.null Or strFileToBeHashed = Nothing Then
+        Return parsedArguments
+    End Function
+
+    Private Sub ColoredConsoleLineWriter(strStringToWriteToTheConsole As String, Optional color As ConsoleColor = ConsoleColor.Green)
+        Console.ForegroundColor = color
+        Console.Write(strStringToWriteToTheConsole)
+        Console.ResetColor()
+    End Sub
+
+    Sub Main()
+        Dim parsedArguments As Dictionary(Of String, Object) = ParseArguments(My.Application.CommandLineArgs)
+        Dim boolAsterisk As Boolean = parsedArguments.ContainsKey("asterisk")
+        Dim strCurrentPath As String = New IO.FileInfo(Process.GetCurrentProcess.MainModule.FileName).DirectoryName
+
+        If parsedArguments.ContainsKey("hash") And parsedArguments.ContainsKey("file") Then
+            Dim strHashMode As String = parsedArguments("hash").ToString.Trim
+            Dim strFileName As String = parsedArguments("file")
+            Dim htHashType As HashType = HashType.null
+
+            If strHashMode.Equals("md5", StringComparison.CurrentCulture) Then
+                htHashType = HashType.md5
+            ElseIf strHashMode.Equals("sha1", StringComparison.CurrentCulture) Then
+                htHashType = HashType.sha160
+            ElseIf strHashMode.Equals("sha160", StringComparison.CurrentCulture) Then
+                htHashType = HashType.sha160
+            ElseIf strHashMode.Equals("sha256", StringComparison.CurrentCulture) Then
+                htHashType = HashType.sha256
+            ElseIf strHashMode.Equals("sha384", StringComparison.CurrentCulture) Then
+                htHashType = HashType.sha384
+            ElseIf strHashMode.Equals("sha512", StringComparison.CurrentCulture) Then
+                htHashType = HashType.sha512
+            ElseIf strHashMode.Equals("ripemd160", StringComparison.CurrentCulture) Then
+                htHashType = HashType.ripemd160
+            Else
+                ColoredConsoleLineWriter("ERROR:", ConsoleColor.Red)
+                Console.WriteLine(" Invalid hash mode.")
+                Exit Sub
+            End If
+
+            If Not IO.Path.IsPathRooted(strFileName) Then strFileName = IO.Path.Combine(strCurrentPath, strFileName)
+
+            If IO.File.Exists(strFileName) Then
+                Dim fileInfo As New IO.FileInfo(strFileName)
+
+                If boolAsterisk Then
+                    Console.WriteLine(ComputeHash(strFileName, htHashType) & " *" & fileInfo.Name)
+                Else
+                    Console.WriteLine(ComputeHash(strFileName, htHashType) & " " & fileInfo.Name)
+                End If
+            Else
+                ColoredConsoleLineWriter("ERROR:", ConsoleColor.Red)
+                Console.WriteLine($" The file ""{strFileName}"" doesn't exist.")
+                Exit Sub
+            End If
+        Else
             Console.WriteLine("You must provide some input for this program to work. For example...")
             Console.WriteLine()
-            Console.WriteLine("checksum.exe -hash=[mode] -file=[filename] -asterisk")
+            Console.WriteLine("checksum.exe --hash=[mode] --file=[filename] --asterisk")
             Console.WriteLine()
             Console.WriteLine("Your hash modes are as follows... crc/crc32 md5 sha1/sha160 sha256 sha384 sha512 ripemd160")
             Console.WriteLine()
             Console.WriteLine("The asterisk flag tells the program if it should include an asterisk before the file name. For example...")
             Console.WriteLine("[checksum] *[file name]")
             Console.WriteLine("If you don't want an asterisk, don't include the flag.")
-        Else
-            Dim fileInfo As New IO.FileInfo(strFileToBeHashed)
-
-            If fileInfo.Exists() Then
-                If boolAsterisk Then
-                    Console.WriteLine(ComputeHash(strFileToBeHashed, htHashType) & " *" & fileInfo.Name)
-                Else
-                    Console.WriteLine(ComputeHash(strFileToBeHashed, htHashType) & " " & fileInfo.Name)
-                End If
-            Else
-                Console.WriteLine("ERROR: The file """ & strFileToBeHashed & """ doesn't exist.")
-            End If
         End If
 
         If Debugger.IsAttached Then Threading.Thread.Sleep(4000)
